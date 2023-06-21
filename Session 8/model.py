bcbd3974eca33416998964fa199a8b2c55d8ea08
mkthoma <mathewkennythomas@gmail.com>
2023-06-21 21:01:34 +0530

    model py updated

--- Session 8/model.py
+++ Session 8/model.py
@@ -382,3 +382,163 @@ def train_test_loss_accuracy(epochs):
     df['test_loss'] = test_loss1
     df['test_accuracy'] = test_accuracy1
     return df
+
+
+
+
+########################################################################################################################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+#######################################################################        SESSION 7       #########################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+
+class session7_model(nn.Module):
+    def __init__(self):
+        super(session7_model, self).__init__()
+        # Input Block
+        self.convblock1 = nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(dropout_value)
+        ) # output_size = 26, RF=3
+
+        # CONVOLUTION BLOCK 1
+        self.convblock2 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 24, RF=5
+
+        # TRANSITION BLOCK 1
+        self.convblock3 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+        ) # output_size = 24, RF=5
+        self.pool1 = nn.MaxPool2d(2, 2) # output_size = 12, RF=6
+
+        # CONVOLUTION BLOCK 2
+        self.convblock4 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(dropout_value)
+        ) # output_size = 10, RF=10
+        self.convblock5 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=10, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(10),
+            nn.Dropout(dropout_value)
+        ) # output_size = 8, RF=14
+        self.convblock6 = nn.Sequential(
+            nn.Conv2d(in_channels=10, out_channels=16, kernel_size=(3, 3), padding=0, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 6, RF=18
+        self.convblock7 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), padding=1, bias=False),
+            nn.ReLU(),
+            nn.BatchNorm2d(16),
+            nn.Dropout(dropout_value)
+        ) # output_size = 6, RF=22
+
+        # OUTPUT BLOCK
+        self.gap = nn.Sequential(
+            nn.AvgPool2d(kernel_size=6)
+        ) # output_size = 1, RF=22
+
+        self.convblock8 = nn.Sequential(
+            nn.Conv2d(in_channels=16, out_channels=10, kernel_size=(1, 1), padding=0, bias=False),
+        ) # output_size = 1, RF=22
+
+
+        self.dropout = nn.Dropout(dropout_value)
+
+    def forward(self, x):
+        x = self.convblock1(x)
+        x = self.convblock2(x)
+        x = self.convblock3(x)
+        x = self.pool1(x)
+        x = self.convblock4(x)
+        x = self.convblock5(x)
+        x = self.convblock6(x)
+        x = self.convblock7(x)
+        x = self.gap(x)
+        x = self.convblock8(x)
+
+        x = x.view(-1, 10)
+        return F.log_softmax(x, dim=-1)
+
+
+########################################################################################################################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+#######################################################################        SESSION 6       #########################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+
+class session6_model(nn.Module):
+    def __init__(self):
+        super(session6_model, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=3, padding=1) 
+        self.bn1 = nn.BatchNorm2d(num_features=8)
+        self.tns1 = nn.Conv2d(in_channels=8, out_channels=4, kernel_size=1, padding=1)
+        self.conv2 = nn.Conv2d(in_channels=4, out_channels=8, kernel_size=3, padding=1) 
+        self.bn2 = nn.BatchNorm2d(num_features=8)  
+        self.pool1 = nn.MaxPool2d(2, 2)   
+        self.conv3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=3, padding=1) 
+        self.bn3 = nn.BatchNorm2d(num_features=16) 
+        self.conv4 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, padding=1) 
+        self.bn4 = nn.BatchNorm2d(num_features=32)
+        self.pool2 = nn.MaxPool2d(2, 2) 
+        self.tns2 = nn.Conv2d(in_channels=32, out_channels=16, kernel_size=1, padding=1)
+        self.conv5 = nn.Conv2d(in_channels=16, out_channels=64, kernel_size=3, padding=1) 
+        self.bn5 = nn.BatchNorm2d(num_features=64) 
+        self.conv6 = nn.Conv2d(in_channels=64, out_channels=10, kernel_size=1, padding=1)    
+        self.gpool = nn.AvgPool2d(kernel_size=7)
+        self.drop = nn.Dropout2d(0.1)
+
+    def forward(self, x):
+        x = self.tns1(self.drop(self.bn1(F.relu(self.conv1(x)))))
+        x = self.drop(self.bn2(F.relu(self.conv2(x))))
+        x = self.pool1(x)
+        x = self.drop(self.bn3(F.relu(self.conv3(x))))        
+        x = self.drop(self.bn4(F.relu(self.conv4(x))))
+        x = self.tns2(self.pool2(x))
+        x = self.drop(self.bn5(F.relu(self.conv5(x))))
+        x = self.conv6(x)
+        x = self.gpool(x)
+        x = x.view(-1, 10)
+        return F.log_softmax(x)
+
+########################################################################################################################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+#######################################################################        SESSION 5      #########################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+########################################################################################################################################################################
+
+class session5_model(nn.Module):
+    def __init__(self):
+        super(session5_model, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, bias=False)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, bias=False)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, bias=False)
+        self.conv4 = nn.Conv2d(128, 256, kernel_size=3, bias=False)
+        self.fc1 = nn.Linear(4096, 50, bias=False)
+        self.fc2 = nn.Linear(50, 10, bias=False)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), 2) 
+        x = F.relu(self.conv3(x), 2)
+        x = F.max_pool2d(F.relu(self.conv4(x)), 2) 
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return F.log_softmax(x, dim=1)
